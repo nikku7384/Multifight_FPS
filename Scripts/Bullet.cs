865cb4fd20280f2886cb6a4c40e7f2728cd93ac6
nikku7384 <47492819+nikku7384@users.noreply.github.com>
2019-02-19 15:08:41 +0530

    Add files via upload

---
+++ Scripts/Bullet.cs
@@ -0,0 +1,42 @@
+﻿     using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float expireRate;
+    public float movespeed;
+    public float damageRate;
+
+    private float currentTimer;
+    private GameObject otherplayer;
+
+  
+    void Update()
+    {
+        this.transform.Translate(Vector3.forward * Time.deltaTime * movespeed);
+        currentTimer += 1 * Time.deltaTime;
+
+        if (currentTimer >= expireRate)
+        {
+            Destroy(this.gameObject);
+
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            otherplayer = other.gameObject;
+            DealDamage(otherplayer);
+        }
+
+    }
+    public void DealDamage(GameObject otherplayer)
+    {
+        otherplayer.GetComponent<player>().Health -= damageRate;
+        Destroy(this.gameObject);
+    }
+
+}
---
+++ Scripts/player.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.FirstPerson;
+
+
+
+
+public class player : NetworkBehaviour
+{
+    public float movingspeed;
+    public GameObject playerCamera;
+    public Transform bulletSpawnPoints;
+    public GameObject bullet;
+    public float Health;
+    private bool triggeringAnotherPlayer;
+    private GameObject otherPlayer;
+
+
+    void Start()
+    {
+        if (isLocalPlayer == true)
+        {
+            playerCamera.GetComponent<Camera>().enabled = true;
+            this.GetComponent<RigidbodyFirstPersonController>().enabled = true;
+        }
+        else
+        {
+            playerCamera.GetComponent<Camera>().enabled = false;
+            this.GetComponent<RigidbodyFirstPersonController>().enabled = false;
+        }
+    }
+
+    void Update() {
+
+        if (isLocalPlayer == true) {
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                this.transform.Translate(Vector3.right * Time.deltaTime * movingspeed);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                this.transform.Translate(Vector3.left * Time.deltaTime * movingspeed);
+            }
+
+            if (triggeringAnotherPlayer && Input.GetKeyDown(KeyCode.E))
+            {
+                Destroy(otherPlayer);
+            }
+            if (Input.GetMouseButtonDown(0)) ;
+            {
+                CmdShoot();
+            }
+        }
+    }
+
+    [Command]
+
+    void CmdShoot()
+    {
+      GameObject _bullet = (GameObject)Instantiate(bullet.gameObject, bulletSpawnPoints.transform.position, Quaternion.identity);
+        _bullet.transform.rotation = bulletSpawnPoints.transform.rotation;
+      NetworkServer.SpawnWithClientAuthority(_bullet, connectionToClient);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+       {
+            triggeringAnotherPlayer = true;
+            otherPlayer = other.gameObject;
+       }
+    }
+
+    void OnTriggerExit(Collider Other)
+    {
+        if(Other.tag == "Player")
+        {
+            triggeringAnotherPlayer = true;
+            otherPlayer = null;
+
+        }
+
+    }
+
+
+  
+
+}
